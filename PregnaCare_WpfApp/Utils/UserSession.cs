6095ac591d4b89b048a60c3bfb6096805be125ec
PTPhong-PPL <PTPhongSWB@gmail.com>
2025-03-31 23:16:05 +0700

    add user session

---
+++ PregnaCare_WpfApp/Utils/UserSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PregnaCare_WpfApp.Utils {
+    public static class UserSession {
+        public static Guid Id { get; set; }
+
+        // call .clear() when logging out
+        public static void Clear() {
+            Id = Guid.Empty;
+        }
+    }
+}
